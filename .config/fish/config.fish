316a9ff0c65c168a9a13048806d05f1ae1bd8096
Michal Szewczak <michaelszewczak@gmail.com>
2016-12-09 15:45:13 +0000

    add config alias to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,5 +8,9 @@ source ~/.asdf/asdf.fish
 set -x CHANGELOG_GITHUB_TOKEN "e6ae2965e587666c1cc4d61b071da21ac426a24d"
 set -x TERM "xterm-256color"
 
-# Prune local git branches that were merged into master alredy
+# Pruning local git branches that were merged into master alredy
 alias gbprune="git branch --merged master | grep -v 'master\$' | xargs git branch -d"
+
+# Alias config
+
+alias config='git --git-dir=$HOME/.dots/ --work-tree=$HOME'
