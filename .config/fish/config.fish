dfb68712fdba24c8427a3d54b02853675fb74149
Michal Szewczak <michaelszewczak@gmail.com>
2016-12-19 13:01:19 +0000

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,15 +5,17 @@ if status --is-login
     end
 end
 source ~/.asdf/asdf.fish
-set -x CHANGELOG_GITHUB_TOKEN (cat $HOME/github.token)
+set -x CHANGELOG_GITHUB_TOKEN (cat $HOME/.keys/github.token)
 set -x TERM "xterm-256color"
 
 # Pruning local git branches that were merged into master alredy
 alias gbprune="git branch --merged master | grep -v 'master\$' | xargs git branch -d"
 
 # Alias config
-
 alias config='git --git-dir=$HOME/.dots/ --work-tree=$HOME'
+
+# Alias keys
+alias keys='/usr/bin/git --git-dir=$HOME/.keys/ '
 
 # autostart tmux
 #if which tmux > /dev/null; and not set -q TMUX
